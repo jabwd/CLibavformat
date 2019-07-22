d7dec4d208612dd0bc399b85160647cdb9a5d65f
Antwan van Houdt <a.vanhoudt@wearetriple.com>
2019-07-22 10:35:00 +0200

    Included avcodec and avutil

--- shim.h
+++ shim.h
@@ -2,5 +2,10 @@
 #define CLIBAV_SHIM_H
 
 #include <libavformat/avformat.h>
+#include <libavutil/avassert.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
+#include <libavutil/mathematics.h>
+#include <libavutil/timestamp.h>
 
 #endif
